6296b1d221c1bf6ece3b5fbca822b057aba79a7c
Hirdesh Mewada <69684968+hirdeshmewada@users.noreply.github.com>
2024-11-22 02:26:11 +0530

    Update users.js

--- models/users.js
+++ models/users.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-    firstName: 'String',
-    lastName : 'String',
-    email: 'String',
-    password: 'String',
-    phoneNumber: 'Number',
-    imageUrl: 'String',
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    phoneNumber: { type: Number, required: true },
+    imageUrl: { type: String, default: '' },  // Optional, defaults to an empty string if not provided
 });
 
-const User = mongoose.model("users", UserSchema);
+const User = mongoose.model("User", UserSchema);
 module.exports = User;
